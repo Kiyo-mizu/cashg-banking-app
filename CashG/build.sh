c9140dab9fdde162df9067d8da7afc1f07e6400b
C.J. Manahan <manahancj16@gmail.com>
2025-08-06 11:14:12 +0800

    Update build.sh

--- CashG/build.sh
+++ CashG/build.sh
@@ -5,8 +5,11 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
-python manage.py collectstatic --noinput
+# Make migrations (in case they don't exist)
+python manage.py makemigrations
 
 # Run database migrations
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
